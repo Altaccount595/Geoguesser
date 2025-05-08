1e3c29242fd6becdc835efb8b473997ae34dc254
Alex Luo <aluo50@stuy.edu>
2025-05-08 10:44:03 -0400

    working flask app

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for, session, request, redirect, jsonify
+import os
+
+app = Flask(__name__)
+
+app.secret_key = os.urandom(32)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
